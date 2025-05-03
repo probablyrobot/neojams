a11538858e1b6c0fa268464c5e4e2b19b89f0b2b
Igor Bogicevic <igor.bogicevic@gmail.com>
2025-05-03 08:42:56 +0200

    Remove old CI workflow and update version formatting in version.py

--- neojams/version.py
+++ neojams/version.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 """Version info for NeoJAMS."""
 
-version = '0.2.0'
+version = "0.2.0"
 
 # Requires Python 3.12 or later
 python_requires = ">=3.12"
